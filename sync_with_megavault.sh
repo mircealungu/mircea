b0a292d00e7ea0b1d2c892015fb4407059c79d5e
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-03-28 18:11:39 +0100

    rename

---
+++ sync_with_megavault.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+rsync -av --delete --exclude='.git/'\
+ --exclude='sync_from_icloud.sh'\
+ --exclude='.gitignore'\
+ --exclude='.obsidian'\
+ --exclude='sync_with_megavault.sh'\
+   "$ML_COM_IN_MEGAVAULT" "$ML_COM_IN_GH_FOLDER"
+
