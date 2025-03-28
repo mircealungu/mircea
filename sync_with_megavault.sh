a70c3954fcbd2f396a706145ee86f289b2a3a65c
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-03-28 18:52:57 +0100

    updated sync script

--- sync_with_megavault.sh
+++ sync_with_megavault.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -f ~/.bash_functions ] && source ~/.bash_functions
+
 
 rsync -av --delete --exclude='.git/'\
  --exclude='sync_from_icloud.sh'\
@@ -9,3 +11,12 @@ rsync -av --delete --exclude='.git/'\
    "$ML_COM_IN_MEGAVAULT" "$ML_COM_IN_GH_FOLDER"
 
 git status
+
+
+echo ""
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+ask_confirmation "Do you want to commit all and push?"
+
+
+git commit -a
+git push 
